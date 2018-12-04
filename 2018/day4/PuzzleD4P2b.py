8553de31ca7a1933b104b643dc51c5a922e433f3
Vladimir Ionita <ionita.vladimir@gmail.com>
2018-12-04 20:47:00 +0100

    Improve day 4 part 2 solution: Add improvements applied to day 4 part 1 solution

--- 2018/day4/PuzzleD4P2b.py
+++ 2018/day4/PuzzleD4P2b.py
@@ -1,61 +1,40 @@
 from utilities import FileUtilities
-from datetime import datetime
 import collections
 
 input_file_path = "puzzle.in"
 file_input = FileUtilities.get_sanitized_content_from_file(input_file_path)
 
+file_input = sorted(file_input)
 
-timeline = []
-for i in file_input:
-    timestamp = i[1:17]
-    instruction = i[19:]
-
-    date = datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
-    timeline.append((date, instruction))
-
-t_ordered = sorted(timeline, key=lambda tup: tup[0])
-
-sleep = collections.defaultdict(int)
+C = collections.defaultdict(lambda: [0 for _ in range(60)])
 g = 0
-s_start = None
-s_end = None
-for k, v in t_ordered:
-    if v.startswith("Guard"):
-        g = v.split()[1]
+t_start = None
+for t in file_input:
+    op = t[19:]
+    if op.startswith("Guard"):
+        g = op.split()[1]
         g = int(g[1:])
-    if v.startswith("falls"):
-        if k.hour != 0:
-            s_start = 0
-        else:
-            s_start = k.minute
+    if op.startswith("falls"):
+        t_start = t[15:17]
+        t_start = int(t_start)
+    if op.startswith("wakes"):
+        t_end = t[15:17]
+        t_end = int(t_end)
 
-    if v.startswith("wakes"):
-        if k.hour != 0:
-            s_end = 0
-        else:
-            s_end = k.minute
-
-        time_asleep = []
-        if g in sleep:
-            time_asleep = sleep[g]
-        else:
-            time_asleep = [0 for x in range(60)]
-
-        for i in range(s_start, s_end):
-            time_asleep[i] += 1
-        sleep[g] = time_asleep
-
+        t_sleep = C[g]
+        for i in range(t_start, t_end):
+            t_sleep[i] += 1
+        C[g] = t_sleep
 
 
 minute_max = 0
 g_max = 0
-for k, v in sleep.items():
+for k, v in C.items():
     m_v = max(v)
     if minute_max < m_v:
         minute_max = m_v
         g_max = k
 
 
-print(g_max, sleep[g_max].index(minute_max))
-print(g_max * sleep[g_max].index(minute_max))
+print(g_max, C[g_max].index(minute_max))
+print(g_max * C[g_max].index(minute_max))
