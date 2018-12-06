ba4dd79f66358095874e96b3a8098f34d6151835
Vladimir Ionita <ionita.vladimir@gmail.com>
2018-12-06 18:35:20 +0100

    Improve day 6 part 1 solution: Add comments for clarity

--- 2018/day6/PuzzleD6P1.py
+++ 2018/day6/PuzzleD6P1.py
@@ -2,12 +2,12 @@ from utilities import FileUtilities
 import collections
 
 
-def get_vertical_distance(c1, c2):
-    return abs(c2[0] - c1[0])
-
-
 def get_horizontal_distance(c1, c2):
     return abs(c2[1] - c1[1])
+
+
+def get_vertical_distance(c1, c2):
+    return abs(c2[0] - c1[0])
 
 
 def get_distance(c1, c2):
@@ -33,13 +33,15 @@ for i in S:
     x, y = int(x), int(y)
     coordinates.append((x, y))
 
-
+# Get coordinates with finite area
 finite = []
 for c in coordinates:
     q1 = []
     q2 = []
     q3 = []
     q4 = []
+
+    # Check what quadrant is the point in
     for c1 in coordinates:
         if c1[0] < c[0] and c1[1] < c[1]:
             q1.append(c1)
@@ -50,9 +52,15 @@ for c in coordinates:
         elif c1[0] > c[0] and c1[1] < c[1]:
             q4.append(c1)
 
+    # If one of the quadrants is empty, the area can extend in that quadrant to infinite
     if not q1 or not q2 or not q3 or not q4:
         continue
 
+    """
+    If the distance between a point C1 and his quadrant axes is smaller than
+    the distance between the coordinate C and the projection of the point C1 on the axes,
+    the point C1 area will block point C area from extending to infinite
+    """
     for c1 in q1:
         if get_horizontal_distance(c, c1) < get_vertical_distance(c, c1):
             continue
@@ -86,9 +94,12 @@ for c in coordinates:
 
 rows, _ = max(coordinates, key=lambda x: x[0])
 _, cols = max(coordinates, key=lambda x: x[1])
+
+# Add a border to the grid
 rows += 1
 cols += 1
 
+# Create the grid
 grid = [[-2 for _ in range(cols)] for _ in range(rows)]
 
 M = collections.defaultdict(int)
@@ -98,7 +109,7 @@ for i in range(rows):
         grid[i][j] = closest_point
         M[closest_point] += 1
 
-
+# Get finite areas
 M1 = {}
 for c in finite:
     M1[coordinates.index(c)] = M[coordinates.index(c)]
