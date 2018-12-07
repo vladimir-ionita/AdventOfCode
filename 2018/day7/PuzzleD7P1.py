165a6526b6d68d16036abef7579c6cf01e398cee
Vladimir Ionita <ionita.vladimir@gmail.com>
2018-12-07 08:20:53 +0100

    Improve day 7 part 1 solution: Make it more clear

--- 2018/day7/PuzzleD7P1.py
+++ 2018/day7/PuzzleD7P1.py
@@ -5,28 +5,26 @@ import collections
 input_file_path = "puzzle.in"
 S = FileUtilities.get_sanitized_content_from_file(input_file_path)
 
-M = collections.defaultdict(list)
-letters = set()
+steps = collections.defaultdict(list)
 for i in S:
     s = i[5]
     d = i[36]
-    M[d].append(s)
-    M[s]
+    steps[d].append(s)
+    steps[s]
 
 
-M = collections.OrderedDict(sorted(M.items()))
+steps = collections.OrderedDict(sorted(steps.items()))
+steps_traveled = []
+while steps:
+    steps_ready = list({k for k, v in steps.items() if len(v) == 0})
+    steps_ready.sort()
+    next_step = steps_ready[0]
+    steps_traveled.append(next_step)
 
-letters = []
-while {k:v for k, v in M.items()}:
-    empty = list({k for k, v in M.items() if len(v) == 0})
-    empty.sort()
-    empty_letter = empty[0] if empty else None
-    letters.append(empty[0])
+    for k, v in steps.items():
+        if next_step in v:
+            steps[k].remove(next_step)
 
-    for k, v in M.items():
-        if empty_letter in v:
-            M[k].remove(empty_letter)
+    steps.pop(next_step)
 
-    M.pop(empty_letter)
-
-print(''.join(letters))
+print(''.join(steps_traveled))
